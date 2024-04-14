dc452fd4308abf834408f4287f8149d9cfa9cff0
JorenSynaeveTM <joren.synaeve@thomasmore.be>
2024-04-14 09:29:48 +0200

    Rework workflows

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+const site = import.meta.env.SITE || "https://jolafun.be/";
+const base = import.meta.env.BASE || "/";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://zenithwebdesign.be/projects/jolafun/",
-  base: "/projects/jolafun/",
+  site,
+  base,
 });
